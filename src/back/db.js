0f9d16033149980ea877095ffadbd8961b8ec053
Rogerio Lima <166033942+RogerioDLima@users.noreply.github.com>
2025-06-26 17:49:19 -0300

    Update db.js

--- src/back/db.js
+++ src/back/db.js
@@ -1,7 +1,5 @@
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
-
-const isProduction = process.env.NODE_ENV === "production";
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.PGDATABASE,
@@ -12,26 +10,13 @@ const sequelize = new Sequelize(
     dialect: process.env.DATABASE_DIALECT,
     port: process.env.PGPORT || 5432,
     logging: false,
-    ...(isProduction && {
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
-    }),
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
   }
 );
 
 module.exports = sequelize;
-
-module.exports = sequelize;
-
-console.log("Variáveis de ambiente carregadas:", {
-  PGDATABASE: process.env.PGDATABASE,
-  PGUSER: process.env.PGUSER,
-  PGPASSWORD: process.env.PGPASSWORD,
-  PGHOST: process.env.PGHOST,
-  PGPORT: process.env.PGPORT,
-  DATABASE_DIALECT: process.env.DATABASE_DIALECT,
-});
